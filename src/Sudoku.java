2abeb0bd3c43fadab5b776be4721bb2f70ed8cf5
victor <victorkrook96@gmail.com>
2020-12-02 08:52:21 +0100

    added class sudoku

---
+++ src/Sudoku.java
@@ -0,0 +1,23 @@
+public class Sudoku implements SudokuSolver {
+
+
+    @Override
+    public boolean solve() {
+        return false;
+    }
+
+    @Override
+    public void randomize() {
+
+    }
+
+    @Override
+    public void setCell(int row, int col, int val) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public int getCell(int row, int col) throws IllegalArgumentException {
+        return 0;
+    }
+}
